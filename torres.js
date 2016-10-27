ec59a951695cfeeb9f1a75fa5248fd63a40e77f8
Michael Saldarriaga <michael91sg@gmail.com>
2016-10-27 10:15:33 -0500

    Add cuadros
    Se han añadido los tres cuadros del juego, los cuales tendran en su interior las
    fichas

--- torres.js
+++ torres.js
@@ -1,8 +1,29 @@
 var cuerpo;
 
+var cuadro1=new cuadro();
+var cuadro2=new cuadro();
+var cuadro3=new cuadro();
+
+function crear_div(){
+  var caja=document.createElement("div");
+  return caja;
+}
+
+function cuadro(){
+  this.caja=crear_div();
+  this.caja.style.width="28%";
+  this.caja.style.height="200px";
+  this.caja.style.marginLeft="4%";
+  this.caja.style.borderWidth="2%";
+  this.caja.style.border="solid black";
+  this.caja.style.float="left";
+}
+
 function iniciar(){
   cuerpo=document.getElementsByTagName("body")[0];
-  alert("Funciona");
+  cuerpo.appendChild(cuadro1.caja);
+  cuerpo.appendChild(cuadro2.caja);
+  cuerpo.appendChild(cuadro3.caja);
 }
 
 window.addEventListener("load",iniciar,false);
