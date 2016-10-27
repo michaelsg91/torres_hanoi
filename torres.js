c86b4f88e94e1694da9ede12f019a737733c35ab
Michael Saldarriaga <michael91sg@gmail.com>
2016-10-27 11:26:52 -0500

    Add methods to see index cards
    Se crearon los metodos para lograr ver las fichas en los cuadros

--- torres.js
+++ torres.js
@@ -1,13 +1,33 @@
 var cuerpo;
 const altura="40px";
 
-var cuadro1=new cuadro();
-var cuadro2=new cuadro();
-var cuadro3=new cuadro();
+var cuadro1=new cuadro(true);
+var cuadro2=new cuadro(false);
+var cuadro3=new cuadro(false);
 
 function crear_div(){
   var caja=document.createElement("div");
   return caja;
+}
+
+function rellenar_contenido(){
+  var contenido=new Array();
+  for(var i=0;i<5;i++){
+    contenido[i]=new relleno();
+  }
+  return contenido;
+}
+
+function rellenar_fichas(){
+  var contenido=new Array();
+
+  contenido[0]=new relleno();
+  contenido[1]=new ficha_s();
+  contenido[2]=new ficha_m();
+  contenido[3]=new ficha_l();
+  contenido[4]=new ficha_xl();
+
+  return contenido;
 }
 
 function ficha_s(){
@@ -49,7 +69,7 @@ function relleno(){
   this.caja.style.height=altura;
 }
 
-function cuadro(){
+function cuadro(caja_inicial){
   this.caja=crear_div();
   this.caja.style.width="28%";
   this.caja.style.height="200px";
@@ -57,6 +77,17 @@ function cuadro(){
   this.caja.style.borderWidth="2%";
   this.caja.style.border="solid black";
   this.caja.style.float="left";
+  this.contenido;
+
+  if(caja_inicial){
+    this.contenido=rellenar_fichas();
+  }else{
+    this.contenido=rellenar_contenido();
+  }
+
+  for(var i=0;i<this.contenido.length;i++){
+    this.caja.appendChild(this.contenido[i].caja);
+  }
 }
 
 function iniciar(){
